db53d985481495773641301d3c4a6d68a204814b
tordvb10 <tordvb10.kodehode@gmail.com>
2024-02-16 14:37:52 +0100

    har fått det obligatoriske til å fungere.

--- 1. Datastruktur-metoder og JSON/1.4. More JSON/json.js
+++ 1. Datastruktur-metoder og JSON/1.4. More JSON/json.js
@@ -1,20 +1,17 @@
 import { jsSounds } from "./jsexample.js";
-import sounds from "./soundJSONexample.json" assert {type: "json"};
+import sounds from "./soundJSONexample.json" assert { type: "json" };
 
+console.log("hello");
+console.log(sounds);
+console.log(jsSounds);
 
+let files = sounds.soundFiles;
 
+console.log(files);
+console.log(files[0].file);
 
-console.log("hello")
-console.log(sounds)
-console.log(jsSounds)
+files.forEach((sound) => console.log(`"Key:" ${sound.key}`));
+files.forEach((sound) => console.log(`"Key:" ${sound.file}`));
 
-let files = sounds.soundFiles
-
-console.log(files)
-// console.log(files[0].file)
-
-files.forEach((sound)=> console.log(`"Key:" ${sound.key}`))
-files.forEach((sound)=> console.log(`"Key:" ${sound.file}`))
-
-jsSounds.forEach((sound)=> console.log(`"Key:" ${sound.key}`))
-jsSounds.forEach((sound)=> console.log(`"Key:" ${sound.file}`))
+jsSounds.forEach((sound) => console.log(`"Key:" ${sound.key}`));
+jsSounds.forEach((sound) => console.log(`"Key:" ${sound.file}`));
